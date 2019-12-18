4885bc112390e7be42909108db41d97ce3e04029
Thuan <ltt.9a1.dtthuan@gmail.com>
2019-12-18 18:10:29 +0700

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+var buttonSaveNew = document.getElementById("save");
+
+function saveNewStudent(){
+     var jsonData =  localStorage.studentList;
+    var studentList = null;
+    if(jsonData == null){
+        studentList = [];
+    }
+    else studentList = JSON.parse(jsonData);
+    var newStudent = {
+        FirstName: '',
+        LastName: '',
+        Year: ''
+    };
+    studentList.push(newStudent);
+    jsonData = JSON.stringify(studentList);
+    localStorage.studentList = jsonData;
+}
+
+buttonSaveNew.addEventListener("click", saveNewStudent())
+//   // read localStorage
+//   var jsonData =     localStorage.studentList;
+//   var studentList = null;
+
+//   if(jsonData == null)
+//    studentList = []; // localStorage null -> new empty array
+//   else
+//   studentList = JSON.parse(jsonData); // localStorage not null -> convert to json
+
+//   // sv moi
+//   var newStuden = {
+//     FirstName: '',
+//     LastName: '',
+//     Dob: ''
+//   };
+
+//   // them sv vao mang
+//   studentList.push(newStuden);
+
+//   // save to localStorage
+//   jsonData = JSON.stringify(studentList);
+//   localStorage.studentList = jsonData;
